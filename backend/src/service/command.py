1ae50cd13308086843bac96ceaf17140a3c451bc
Рафаэль Агишев <kekrafael@yandex.ru>
2024-11-25 10:40:36 +0500

    speach2text

--- backend/src/service/command.py
+++ backend/src/service/command.py
@@ -1,26 +1,45 @@
-import io
+from io import BytesIO
+from time import time
 
-import speech_recognition as sr
-from speech_recognition import AudioFile
+from faster_whisper import WhisperModel
 
 
 class SpeachToText:
     def __init__(self) -> None:
-        self.r = sr.Recognizer()
+        pass
 
-    def convert(self, file_content: bytes) -> str:
-        audio_stream = io.BytesIO(file_content)
-        print(audio_stream)
+    def convert(self, file_content: BytesIO, lang: str = "ru") -> str:
+        model_size = "medium"
+        # model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-        with AudioFile(audio_stream) as source:
-            audio_data = self.r.record(source)
+        start = time()
+        model = WhisperModel(model_size, device="cpu", compute_type="int8")
+        end = time()
+        print(f"[LOG]: Init model: {end-start} s.")
+        start = time()
+        segments, info = model.transcribe(file_content, beam_size=5, language="ru")
+        end = time()
+        print(f"[LOG]: Get segments: {end-start} s.")
 
-        text = self.r.recognize_google_cloud(audio_data)
-        return text
+        total_text = ""
+        i = 1
+        for segment in segments:
+            start = time()
+            total_text += "[%.2fs -> %.2fs] %s\n" % (
+                segment.start,
+                segment.end,
+                segment.text,
+            )
+            end = time()
+            print(f"[LOG]: Get segment {i}: {end-start} s.")
+            i += 1
 
-    def get_file_content(self, filename: str) -> bytes:
+        return total_text
+
+    def get_file_content(self, filename: str) -> BytesIO:
         with open(filename, "rb") as file:
-            return file.read()
+            file_content = BytesIO(file.read())
+            return file_content
 
 
 if __name__ == "__main__":
